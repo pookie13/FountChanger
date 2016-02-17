9629f71f923fb32de28b323601df9c6fe0bcbb30
pookie piyush <piyush.gupta202390@gmail.com>
2016-02-17 20:02:49 +0530

    Create FontChanger.java
    Add fount changer java  class into projects

---
+++ utils/FontChanger.java
@@ -0,0 +1,37 @@
+/**
+*create by windows(21/12/2015)
+*/
+
+public class FontChanger
+{
+    private Typeface typeface;
+
+    public FontChanger(Typeface typeface)
+    {
+        this.typeface = typeface;
+    }
+
+    public FontChanger(AssetManager assets, String assetsFontFileName)
+    {
+        typeface = Typeface.createFromAsset(assets, assetsFontFileName);
+    }
+
+    public void replaceFonts(ViewGroup viewTree)
+    {
+        View child;
+        for(int i = 0; i < viewTree.getChildCount(); ++i)
+        {
+            child = viewTree.getChildAt(i);
+            if(child instanceof ViewGroup)
+            {
+                // recursive call
+                replaceFonts((ViewGroup)child);
+            }
+            else if(child instanceof TextView)
+            {
+                // base case
+                ((TextView) child).setTypeface(typeface);
+            }
+        }
+    }
+}
